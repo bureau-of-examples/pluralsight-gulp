0130b7e799a4cb9614328115831e7e0bb30afad6
zhy2002 <hz141@uowmail.edu.au>
2015-12-24 23:24:49 +1100

    Added jshint and jscs with gulp-if and gulp-print.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var util = require('gulp-util');
+var gulpPrint = require('gulp-print');
+var gulpIf = require('gulp-if');
+var args = require('yargs').argv;
 
-gulp.task('hello-world', function(){
-	console.log('Our first hello world gulp task!');
+/* jshint ignore:start */
+var abc = 111
+/* jshint ignore:end */
+
+gulp.task('vet', function(){
+    log('Running jshint and jscs...');
+    return gulp.src(['./src/**/*.js', './*.js'])
+        .pipe(gulpIf(args.verbose, gulpPrint()))
+        .pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe(jshint.reporter('fail'));
 });
+
+
+///////////////////////
+function log(msg){
+    if(typeof(msg) === 'object'){
+        for(var item in msg){
+            if(msg.hasOwnProperty(item)){
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(msg));
+    }
+}
